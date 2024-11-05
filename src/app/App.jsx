759745fba7c26891b412d91577d639dbf4ecd576
andrea <103574857+andrea-andrenucci@users.noreply.github.com>
2024-11-05 16:57:09 +0100

    changed to obj

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,11 +4,14 @@ import React from "react";
 const PdfViewer = ({ pdfUrl }) => {
   return (
     <div className=".pdf-viewer">
-      <iframe
+      {/* <iframe
         src={pdfUrl}
         allowFullScreen
         title="PDF Viewer"
-      ></iframe>
+      ></iframe> */}
+      <object data={pdfUrl} type="application/pdf" width="100%" height="100%">
+        <p>Il PDF non può essere visualizzato.</p>
+      </object>
     </div>
   );
 };
@@ -16,7 +19,7 @@ const PdfViewer = ({ pdfUrl }) => {
 const App = () => {
   return (
     <>
-      <div className='fixed z-50 w-full h-16 bg-red-600'/>
+      {/* <div className='fixed z-50 w-full h-16 bg-red-600'/> */}
       <div className="h-screen">
         <PdfViewer pdfUrl={'/preventivo_buro.pdf'} />
       </div>
